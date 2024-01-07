50ca6bcfc1122298e9f33f4b7e841f901639350e
swizz <emailswizz@gmail.com>
2024-01-07 13:50:09 -0500

    okok

--- pages/index.js
+++ pages/index.js
@@ -171,21 +171,25 @@ export default function Home() {
         <table className="min-w-full table-auto">
           <thead className="bg-blue-100">
             <tr className="text-left text-gray-700 uppercase text-sm leading-normal">
-              <th className="py-3 px-6">Call ID</th>
-              <th className="py-3 px-6">Created At</th>
-              <th className="py-3 px-6 text-center">Call Length</th>
-              <th className="py-3 px-6 text-center">To</th>
+            <th className="py-3 px-6 text-center">To</th>
               <th className="py-3 px-6 text-center">From</th>
+              <th className="py-3 px-6 text-center">Call Length</th>
+              <th className="py-3 px-6">Created At</th>
+              <th className="py-3 px-6">Call ID</th>
+      
+          
+       
             </tr>
           </thead>
           <tbody className="text-gray-600 text-sm font-light">
           {calls.map((call, index) => (
             <tr key={index} className="border-b border-gray-200 hover:bg-gray-50">
-              <td className="py-3 px-6">{call.c_id}</td>
-              <td className="py-3 px-6">{formatCreatedAt(call.created_at)}</td>
-              <td className="py-3 px-6 text-center">{formatCallLength(call.call_length)}</td>
-              <td className="py-3 px-6 text-center">{call.to}</td>
+                    <td className="py-3 px-6 text-center">{call.to}</td>
               <td className="py-3 px-6 text-center">{call.from}</td>
+              <td className="py-3 px-6 text-center">{formatCallLength(call.call_length)}</td>
+              <td className="py-3 px-6">{formatCreatedAt(call.created_at)}</td>
+              <td className="py-3 px-6">{call.c_id}</td>
+
             </tr>
           ))}
         </tbody>
